08d9eebf087e7ccbc23857d0d21d2fce71c0d1d4
bassel-hammad <bassel.shahat03@eng-st.cu.edu.eg>
2025-02-26 12:23:41 +0200

    lab3

---
+++ Lab#3/JS Async[students].js
@@ -0,0 +1,48 @@
+// --------------------------------------------------------------------
+// PROMISES
+// --------------------------------------------------------------------
+
+const processOrderPromise = (orderId) => {
+  return new Promise((resolve, reject) => {
+    if (!orderId || typeof orderId !== "number") {
+      reject("Invalid order ID");
+      return;
+    } else {
+      setTimeout(() => {
+        console.log("Fetched order details for order ID:", orderId);
+        resolve({ orderId, status: "Processed" });
+      }, 1000);
+    }
+  });
+};
+
+// Call processOrderPromise() properly to console log the returned order details and catch any errors
+processOrderPromise(100);
+  
+
+// Call processOrderPromise() with an invalid order ID to console log the error message
+processOrderPromise('invalid')
+  .then((orderDetails) => {
+    console.log("Order details:", orderDetails);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+// --------------------------------------------------------------------
+// ASYNC/AWAIT
+// --------------------------------------------------------------------
+
+const processOrderAwait = async (orderId) => {
+  try {
+    const orderDetails = await processOrderPromise(orderId);
+    console.log("Order details (Async/Await):", orderDetails);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// Call processOrderAwait()
+processOrderAwait(200);
+
+processOrderAwait("invalid");
